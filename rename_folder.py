623778f5bbdf9b95777c43a1fe47294feac19a50
Eren <julian07027@gmail.com>
2017-11-18 23:10:36 +0800

    Modified rename_folder

--- rename_folder.py
+++ rename_folder.py
@@ -1,15 +1,10 @@
 
 # coding: utf-8
 
-# 使用正規表示法去除資料夾名稱的瑣碎字串，並重新命名
-
-# 比對規則：
-# 找出資料夾名稱的格式為 '[123456] ' 開頭，
-# 中括號內為 6 位數字，比對後取代為空字串（如同刪除 '[123456] ' 字串）。
+# 比對規則：找出資料夾名稱的格式為 '[123456] ' 開頭，中括號內為 6 位數字，比對後取代為空字串。
 
 import os
 import re
-
 for fn in os.listdir('.'):
     try:
         match = re.sub(r'^\[\d{6}\]\s', '',fn)
@@ -17,6 +12,4 @@ for fn in os.listdir('.'):
         os.rename(fn, match)
     except PermissionError as e:
         print(e)
-        break
-    else:
         pass
