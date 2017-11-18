2cecbd60a7d7520be4cc78b79a97d6eac670e47a
Eren <julian07027@gmail.com>
2017-11-18 23:35:45 +0800

    Modified rename_folder, Code: 整理MP3資料夾

--- rename_folder.py
+++ rename_folder.py
@@ -1,14 +1,29 @@
 
 # coding: utf-8
 
-# 比對規則：找出資料夾名稱的格式為 '[123456] ' 開頭，中括號內為 6 位數字，比對後取代為空字串。
+# 開頭比對
+# 找出資料夾名稱的開頭格式為 '[123456] '
+# 中括號內為 6 位數字，比對後取代為空字串。
 
 import os
 import re
 for fn in os.listdir('.'):
     try:
         match = re.sub(r'^\[\d{6}\]\s', '',fn)
-        print(match)
+        print("[INFO] 成功去除開頭格式：" + match)
+        os.rename(fn, match)
+    except PermissionError as e:
+        print(e)
+        pass
+
+# 結尾比對
+# 找出資料夾名稱的結尾格式為 ' [320K+BK]' 或 ' [320K]' 或 ' [MP3]'
+# 比對後取代為空字串。
+
+for fn in os.listdir('.'):
+    try:
+        match = re.sub(r'\s\[(320K\+BK|320K|MP3)\]$', '',fn)
+        print("[INFO] 成功去除結尾格式：" + match)
         os.rename(fn, match)
     except PermissionError as e:
         print(e)
