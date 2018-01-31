4400986e884cf7f1e2138591f3c0bde3482d4b2f
Eren <julian07027@gmail.com>
2018-01-31 18:28:20 +0800

    Modified rename_folder.py

--- rename_folder.py
+++ rename_folder.py
@@ -1,23 +1,11 @@
-
 # coding: utf-8
+
+import os
+import re
 
 # 開頭比對
 # 找出資料夾名稱的開頭格式為 '[123456] '
 # 中括號內為 6 位數字，比對後取代為空字串。
-
-import os
-import re
-for fn in os.listdir('.'):
-    try:
-        match = re.sub(r'^\[\d{6}\]\s', '',fn)
-        print("[INFO] 成功去除開頭格式：" + match)
-        os.rename(fn, match)
-    except PermissionError as e:
-        print(e)
-        pass
-    except FileExistsError as e:
-        print(e)
-        pass
 
 # 結尾比對
 # 找出資料夾名稱的結尾格式為 ' [320K+BK]' 或 ' [320K]' 或 ' [MP3]'
@@ -25,9 +13,15 @@ for fn in os.listdir('.'):
 
 for fn in os.listdir('.'):
     try:
-        match = re.sub(r'\[(320K\+BK|320K|MP3)\]$', '',fn)
-        print("[INFO] 成功去除結尾格式：" + match)
+        match = re.sub(r'^\[\d{6}\]\s', '',fn)
         os.rename(fn, match)
+        print("[INFO] 成功去除開頭格式：" + match)
+
+        match2 = re.sub(r'」／.*', '」',fn)
+        os.rename(fn, match2)
+        match3 = re.sub(r'\[(320K\+BK|320K|MP3)\]$', '',fn)
+        os.rename(fn, match3)
+        print("[INFO] 成功去除結尾格式：" + match3)
     except PermissionError as e:
         print(e)
         pass
