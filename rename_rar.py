02a29841f40e33cf453e9ef4a1562290c48bb414
Eren <julian07027@gmail.com>
2018-02-04 11:23:34 +0800

    Modified rename_rar

--- rename_rar.py
+++ rename_rar.py
@@ -5,7 +5,7 @@ import re
 
 for fn in os.listdir('.'):
     try:
-        match = re.sub(r'rar.*', 'rar',fn)
+        match = re.sub(r'\.rar.*', '.rar',fn)
         os.rename(fn, match)
         print("[INFO] 成功去除格式：" + match)
     except PermissionError as e:
