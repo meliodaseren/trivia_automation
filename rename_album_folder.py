8ca8befb21d50b02325f3932686cd48b859b3e25
Eren <julian07027@gmail.com>
2019-01-06 17:25:57 +0800

    Update codes

--- rename_album_folder.py
+++ rename_album_folder.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from os import rename, listdir
+import os
+import shutil
 from re import sub
 
 # 開頭比對
@@ -12,16 +13,24 @@ from re import sub
 # 找出資料夾名稱的結尾格式為 ' [320K+BK]' 或 ' [320K]' 或 ' [MP3]'
 # 比對後取代為空字串。
 
-folder_path = 'E:\BitComet Downloads'
+#folder_path = 'E:\BitComet Downloads'
+folder_path = '.'
+files = os.listdir(folder_path)
+rar_backup_path = 'E:\BitComet Downloads\RAR_Backup'
 
-for fn in listdir(folder_path):
-    print(fn)
+if not os.path.exists('E:\BitComet Downloads\RAR_Backup'):
+    os.makedirs('E:\BitComet Downloads\RAR_Backup')
 
-for fn in listdir(folder_path):
+for file in files:
+    if file.endswith('.rar'):
+        shutil.move(os.path.join(folder_path, file), os.path.join(rar_backup_path, file))
+
+'''
+for fn in os.listdir(folder_path):
     try:
         # [181017]
         match = sub(r'^\[\d{6}\]', '',fn)
-        rename(fn, match)
+        os.rename(fn, match)
         print("[INFO] 成功去除開頭之日期格式：" + match)
     except PermissionError as e:
         print(e)
@@ -30,11 +39,11 @@ for fn in listdir(folder_path):
         print(e)
         pass
 
-for fn in listdir(folder_path):
+for fn in os.listdir(folder_path):
     try:
         # [2016.10.28]
         match = sub(r'^\[\d{4}\.\d{2}\.\d{2}\]', '',fn)
-        rename(fn, match)
+        os.rename(fn, match)
         print("[INFO] 成功去除開頭之日期格式：" + match)
     except PermissionError as e:
         print(e)
@@ -43,12 +52,12 @@ for fn in listdir(folder_path):
         print(e)
         pass
 
-for fn in listdir(folder_path):
+for fn in os.listdir(folder_path):
     try:
         # [320K+BK], [320K], [MP3]
         match2 = sub(r'\[(320K\+BK|320K|MP3|MP3\s320K|MP3\s320K\+BK)\]$', '',fn)
         match2 = sub(r'\[(320K.*|MP3.*)\]$', '',fn)        
-        rename(fn, match2)
+        os.rename(fn, match2)
         print("[INFO] 成功去除後綴格式：" + match2)
     except PermissionError as e:
         print(e)
@@ -58,11 +67,11 @@ for fn in listdir(folder_path):
         pass
 
 
-for fn in listdir(folder_path):
+for fn in os.listdir(folder_path):
     try:
         # 」／.*
         match3 = sub(r'」／.*', '」',fn)
-        rename(fn, match3)
+        os.rename(fn, match3)
         print("[INFO] 成功去除後綴之歌手資訊：" + match3)
     except PermissionError as e:
         print(e)
@@ -70,3 +79,4 @@ for fn in listdir(folder_path):
     except FileExistsError as e:
         print(e)
         pass
+'''
