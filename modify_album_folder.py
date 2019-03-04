5588c1287bb056467322217163e95efd67b961d5
Eren <julian07027@gmail.com>
2019-03-04 22:22:27 +0800

    Update the code for album folder modification

--- modify_album_folder.py
+++ modify_album_folder.py
@@ -14,17 +14,25 @@ for dir in dirs:
 #    print("[INFO]", dir_path)
 
     # NOTE: [Stage 1] Check the *.url in folder
+    print("[Stage 1] Check the *.url in folder")
     url_path = f"{dir_path}\\*.url"
     url_glob = glob.glob(url_path)
+    for _ in url_glob:
+        os.remove(_)
 #    print("[INFO]", url_glob)
 
     # NOTE: [Stage 2] Move MP3 file to previous folder
+    print("[Stage 2] Move MP3 file to previous folder, and remove empty folder")
     mp3_folder = f"{folder_path}\\{dir}\\MP3"
     if os.path.isdir(mp3_folder):
 #        print("[INFO] MP3 folder is exist", mp3_folder)
         file_in_mp3_folder_list = os.listdir(mp3_folder)
-#        print("[INFO]", file_in_mp3_folder_list)
         for f in file_in_mp3_folder_list:
             filename = f"{mp3_folder}\\{f}"
             shutil.move(filename, dir_path)
         os.removedirs(mp3_folder)
+
+    # NOTE: [Stage 3] Replace『』to「」
+    print("[Stage 3] Replace『』to「」")
+    dir_new = dir.replace("『", "「").replace("』", "」")
+    os.rename(f"{folder_path}\\{dir}", f"{folder_path}\\{dir_new}")
