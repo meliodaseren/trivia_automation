5e2722911bac65c79c8ab7edc45e27fe84951fe3
Eren <dn0306@hotmail.com>
2021-02-04 22:53:54 +0800

    Fix zip

--- src/zip_jpg_to_cbz.py
+++ src/zip_jpg_to_cbz.py
@@ -26,7 +26,7 @@ def zip_all_jpg(path):
             os.chdir(dpath)
             for file in os.listdir(dpath):
                 if '.png' in file or '.jpg' in file:
-                    file = f'{dpath}\\{file}'
+                    # file = f'{dpath}\\{file}'
                     print(f"zip {file} to {out_cbz}")
                     output_zip.write(os.path.join('.', file), file, zipfile.ZIP_DEFLATED)
             os.chdir(path)
@@ -38,5 +38,5 @@ def zip_all_jpg(path):
 
 if __name__ == '__main__':
     # folder_path = input("Please enter your path: ")
-    path = 'E:\\kobo ebook'
-    zip_all_jpg(path)
+    folder_path = 'E:\\kobo ebook'
+    zip_all_jpg(folder_path)
